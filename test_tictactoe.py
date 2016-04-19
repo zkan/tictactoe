d07b4bd14c7555db423f41aa4c015e5dac0315f9
zkan <kan@prontomarketing.com>
2016-04-19 14:59:51 +0700

    Introduce TicTacToe class and put display_board in

--- test_tictactoe.py
+++ test_tictactoe.py
@@ -2,22 +2,22 @@ from io import BytesIO as StringIO
 from mock import patch
 import unittest
 
-import tictactoe
+from tictactoe import TicTacToe
 
 
 class TicTacToeTest(unittest.TestCase):
     def test_display_board_should_print_board_to_stdout(self):
         with patch('sys.stdout', new=StringIO()) as fake_output:
-            board = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
-            tictactoe.display_board(board)
+            game = TicTacToe()
+            game.display_board()
             expected = '     |     |     \n'
-            expected += '  1  |  2  |  3  \n'
+            expected += '     |     |     \n'
             expected += '_____|_____|_____\n'
             expected += '     |     |     \n'
-            expected += '  4  |  5  |  6  \n'
+            expected += '     |     |     \n'
             expected += '_____|_____|_____\n'
             expected += '     |     |     \n'
-            expected += '  7  |  8  |  9  \n'
+            expected += '     |     |     \n'
             expected += '     |     |     \n'
             self.assertEqual(fake_output.getvalue(), expected)
 
--- tictactoe.py
+++ tictactoe.py
@@ -1,10 +1,20 @@
-def display_board(board):
-    print "     |     |     "
-    print "  %s  |  %s  |  %s  " % (board[0], board[1], board[2])
-    print "_____|_____|_____"
-    print "     |     |     "
-    print "  %s  |  %s  |  %s  " % (board[3], board[4], board[5])
-    print "_____|_____|_____"
-    print "     |     |     "
-    print "  %s  |  %s  |  %s  " % (board[6], board[7], board[8])
-    print "     |     |     "
+class TicTacToe:
+    def __init__(self):
+        self.board = [' '] * 9
+
+    def display_board(self):
+        print "     |     |     "
+        print "  %s  |  %s  |  %s  " % (self.board[0],
+                                        self.board[1],
+                                        self.board[2])
+        print "_____|_____|_____"
+        print "     |     |     "
+        print "  %s  |  %s  |  %s  " % (self.board[3],
+                                        self.board[4],
+                                        self.board[5])
+        print "_____|_____|_____"
+        print "     |     |     "
+        print "  %s  |  %s  |  %s  " % (self.board[6],
+                                        self.board[7],
+                                        self.board[8])
+        print "     |     |     "
